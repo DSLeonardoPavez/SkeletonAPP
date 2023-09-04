74079d7f4152ab38ad8bc4594e64087b7544e24e
leon death <leonardo.pavez.06@gmail.com>
2023-09-04 12:58:45 -0300

    comit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage {
+
+  username: string = ''; 
+  password: string = ''; 
+
+  constructor() {}
+
+  login() {
+    
+  }
+}
+
